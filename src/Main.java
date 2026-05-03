43df2d40055a241055a9561e7df60a0587b8dca9
Vinicius Pedon <viniciuspedon9@gmail.com>
2026-05-03 09:27:09 -0300

    Transformei o cadastro em meotodos com tarefas claras e unicas

--- src/Main.java
+++ src/Main.java
@@ -31,10 +31,106 @@ public class Main{
 
         return OpComando;
     }
+    //Metodos de criação de usuario.
+    static String nome() {
+        Scanner leia = new Scanner(System.in);
 
-    public static void main(String[] args) {
+        String nomeCompleto;
+        System.out.print("Nome completo: ");
+        nomeCompleto = leia.nextLine();
+
+        return nomeCompleto;
+    }
+    static String cpf() {
         Scanner leia = new Scanner(System.in);
         ValidadorDeFormatos validador = new ValidadorDeFormatos();
+
+        String cpf;
+        System.out.print("CPF (siga o formato: XXX.XXX.XXX-XX): ");
+        cpf = leia.nextLine();
+
+        var validarCPF = validador.validarCPF(cpf);
+        if (!validarCPF) {
+            System.out.println("Formato invalido!");
+        }
+        while (!validarCPF) {
+            System.out.print("CPF (siga o formato: XXX.XXX.XXX-XX): ");
+            cpf = leia.nextLine();
+            validarCPF = validador.validarCPF(cpf);
+            if (!validarCPF) {
+                System.out.println("Formato invalido!");
+            }
+        }
+
+        return cpf;
+    }
+    static String number() {
+        Scanner leia = new Scanner(System.in);
+        ValidadorDeFormatos validador = new ValidadorDeFormatos();
+
+        String numero;
+        System.out.print("Numero de contato (siga o formato: (xx)xxxxx-xxxx): ");
+        numero = leia.nextLine();
+        var validarCelular = validador.validarCelular(numero);
+        while (!validarCelular) {
+            System.out.println("Invalido!");
+            System.out.print("Numero de contato (siga o formato: (xx)xxxxx-xxxx): ");
+            numero = leia.nextLine();
+            validarCelular = validador.validarCelular(numero);
+        }
+        return numero;
+    }
+    static String email() {
+        Scanner leia = new Scanner(System.in);
+        ValidadorDeFormatos validador = new ValidadorDeFormatos();
+
+        String email;
+        System.out.print("Email: ");
+        email = leia.nextLine();
+        var validarEmail = validador.validarEmail(email);
+        while (!validarEmail){
+            System.out.println("Email invalido!");
+            System.out.print("Email: ");
+            email = leia.nextLine();
+            validarEmail = validador.validarEmail(email);
+        }
+        return email;
+    }
+    static String senha(){
+        ValidadorDeFormatos validador = new ValidadorDeFormatos();
+        Scanner leia = new Scanner(System.in);
+
+        String senha;
+        System.out.print("Senha: ");
+        senha = leia.nextLine();
+        var validarSenha = validador.validarSenha(senha);
+        while (!validarSenha) {
+            System.out.print("Senha: ");
+            senha = leia.nextLine();
+            validarSenha = validador.validarSenha(senha);
+        }
+
+        String senhaDeConfirmacao;
+        senhaDeConfirmacao = "";
+
+        int contador;
+        contador = 0;
+        while (!senha.equals(senhaDeConfirmacao)) {
+            if (contador == 0) {
+                System.out.print("Digite a senha novamente: ");
+                senhaDeConfirmacao = leia.nextLine();
+            }
+            else {
+                System.out.println("ERRO! as senhas digitadas não são correspondentes.");
+                System.out.print("Digite a senha novamente: ");
+                senhaDeConfirmacao = leia.nextLine();
+            }
+            contador++;
+        }
+        return senha;
+    }
+
+    public static void main(String[] args) {
 
         cabecalho();
 
@@ -44,76 +140,16 @@ public class Main{
             int OpComando = loginPage();
 
             if (OpComando == 1) {
-                String nomeCompleto;
-                System.out.print("Nome completo: ");
-                nomeCompleto = leia.nextLine();
 
-                String cpf;
-                System.out.print("CPF (siga o formato: XXX.XXX.XXX-XX): ");
-                cpf = leia.nextLine();
+                String nomeCompleto = nome();
 
-                var validarCPF = validador.validarCPF(cpf);
-                if (!validarCPF) {
-                    System.out.println("Formato invalido!");
-                }
-                while (!validarCPF) {
-                    System.out.print("CPF (siga o formato: XXX.XXX.XXX-XX): ");
-                    cpf = leia.nextLine();
-                    validarCPF = validador.validarCPF(cpf);
-                    if (!validarCPF) {
-                        System.out.println("Formato invalido!");
-                    }
-                }
+                String cpf = cpf();
 
-                String numero;
-                System.out.print("Numero de contato (siga o formato: (xx)xxxxx-xxxx): ");
-                numero = leia.nextLine();
-                var validarCelular = validador.validarCelular(numero);
-                while (!validarCelular) {
-                    System.out.println("Invalido!");
-                    System.out.print("Numero de contato (siga o formato: (xx)xxxxx-xxxx): ");
-                    numero = leia.nextLine();
-                    validarCelular = validador.validarCelular(numero);
-                }
+                String numero = number();
 
-                String email;
-                System.out.print("Email: ");
-                email = leia.nextLine();
-                var validarEmail = validador.validarEmail(email);
-                while (!validarEmail){
-                    System.out.println("Email invalido!");
-                    System.out.print("Email: ");
-                    email = leia.nextLine();
-                    validarEmail = validador.validarEmail(email);
-                }
+                String email = email();
 
-                String senha;
-                System.out.print("Senha: ");
-                senha = leia.nextLine();
-                var validarSenha = validador.validarSenha(senha);
-                while (!validarSenha) {
-                    System.out.print("Senha: ");
-                    senha = leia.nextLine();
-                    validarSenha = validador.validarSenha(senha);
-                }
-
-                String senhaDeConfirmacao;
-                senhaDeConfirmacao = "";
-
-                int contador;
-                contador = 0;
-                while (!senha.equals(senhaDeConfirmacao)) {
-                    if (contador == 0) {
-                        System.out.print("Digite a senha novamente: ");
-                        senhaDeConfirmacao = leia.nextLine();
-                    }
-                    else {
-                        System.out.println("ERRO! as senhas digitadas não são correspondentes.");
-                        System.out.print("Digite a senha novamente: ");
-                        senhaDeConfirmacao = leia.nextLine();
-                    }
-                    contador++;
-                }
+                String senha = senha();
 
                 var conta = CriadorDeContas.criarConta();
                 var digito = CriadorDeContas.criarDigito();
@@ -126,6 +162,5 @@ public class Main{
                 break;
             }
         }
-        leia.close();
     }
 }
